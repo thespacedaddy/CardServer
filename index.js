a74c9a931df1e34a94e898dd945de36af1f32993
Jaxcksn <jcasey396@gmail.com>
2020-02-26 17:53:16 +0000

    Added Socket and Player Connection

---
+++ index.js
@@ -0,0 +1,50 @@
+//Stuff to Create the Server
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+const port = 3000
+
+//Socket IO Stuff
+var io = require('socket.io')(http)
+
+//Other Dependencies
+const cnc = require('chance')
+const chance = new cnc();
+const idPool =  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()[]'
+
+
+
+//Other Variables
+
+
+
+/**
+ * Socket IO Management:
+ *  
+ * 
+ * 
+ */
+io.on('connection', (socket) => {
+  //When the player joins the game.
+  socket.on('playerJoin', (choices) => {
+    //generate a GUID
+    let playerID = chance.guid()
+    //generate the player JSON file
+    let playerJSON = {
+      playerID: playerID,
+      playerType: choices.playerType,
+      playerNickname: choices.playerNickname,
+    }
+
+    console.log(playerJSON)
+  })
+});
+
+
+
+
+
+// Start HTTP server
+http.listen(3000, function() {
+  console.log('listening on 3000');
+});
